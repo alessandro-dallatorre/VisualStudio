4d03cf65418ae5b15040ebffa3c1c7aaaa1b13d6
PC14-528\Alessandro <alessandro.dallatorre@post.lu>
2016-09-14 10:35:11 +0200

    Log4net added

--- SQLiteEncrypted/Program.cs
+++ SQLiteEncrypted/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
+using log4net.Core;
 using SQLite.Net.Cipher.Interfaces;
 using SQLite.Net.Cipher.Model;
 using System;
@@ -21,57 +22,72 @@ namespace SQLiteEncrypted
 
     class Program
     {
+        private static readonly log4net.ILog log =
+    log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
-            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            var connectionStringsSection = (ConnectionStringsSection)config.GetSection("connectionStrings");
-            string keySeed;
-            const string baseFolder = @"C:\SQLITEDATABASES";
-            var dbFilePath = baseFolder + @"\SQLITEDB1.sqlite";
-
             try
             {
-                var connString = connectionStringsSection.ConnectionStrings["SQLLiteDbconnection"].ConnectionString;
-                Dictionary<string, string> connStringParts = connString.Split(';')
-               .Select(t => t.Split(new char[] { '=' }, 2))
-               .ToDictionary(t => t[0].Trim(), t => t[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
-                dbFilePath = connStringParts["Data Source"];
-                keySeed = connStringParts["Password"];
+                log.Info("Application is working");
+                var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var connectionStringsSection = (ConnectionStringsSection) config.GetSection("connectionStrings");
+                string keySeed;
+                const string baseFolder = @"C:\SQLITEDATABASES";
+                var dbFilePath = baseFolder + @"\SQLITEDB1.sqlite";
+
+                try
+                {
+                    var connString = connectionStringsSection.ConnectionStrings["SQLLiteDbconnection"].ConnectionString;
+                    Dictionary<string, string> connStringParts = connString.Split(';')
+                        .Select(t => t.Split(new char[] {'='}, 2))
+                        .ToDictionary(t => t[0].Trim(), t => t[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
+                    dbFilePath = connStringParts["Data Source"];
+                    keySeed = connStringParts["Password"];
+                    log.Info("Seed retrieved from encrypted section");
+                }
+                catch (Exception ex)
+                {
+                    log.Info(string.Format("Seed missing from encrypted section: {0}",ex.Message));
+                    keySeed = GenerateConnectionStringSectionWithSeed(config, dbFilePath);
+                    log.Info("Seed generated and section encrypted.");
+                }
+
+                if (string.IsNullOrEmpty(keySeed))
+                {
+                    keySeed = GenerateConnectionStringSectionWithSeed(config, dbFilePath);
+                    log.Info("Seed generated and section encrypted.");
+                }
+
+                var platform = new SQLite.Net.Platform.Win32.SQLitePlatformWin32();
+
+                Directory.CreateDirectory(baseFolder);
+
+                ISecureDatabase database = new MyDatabase(platform, dbFilePath);
+                log.Info("SQLite DB opened.");
+
+                var user = new SampleUser()
+                {
+                    Name = "Has AlTaiar",
+                    Password = "very secure passwor, my dear Alex :)",
+                    Id = Guid.NewGuid().ToString()
+                };
+
+                var inserted = database.SecureInsert(user, keySeed);
+                log.Info("SQLite record inserted.");
+                var got = database.SecureGetAll<SampleUser>(keySeed);
+                log.Info("SQLite read succeeded.");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                keySeed = GenerateConnectionStringSectionWithSeed(config, dbFilePath);  
+                log.Fatal(string.Format("{0} - {1}", ex.Message, ex.StackTrace));
             }
-
-            if (string.IsNullOrEmpty(keySeed))
-            {
-                keySeed = GenerateConnectionStringSectionWithSeed(config, dbFilePath);
-            }
-
-            var platform = new SQLite.Net.Platform.Win32.SQLitePlatformWin32();
-
-            Directory.CreateDirectory(baseFolder);
-
-            ISecureDatabase database = new MyDatabase(platform, dbFilePath);
-
-
-            var user = new SampleUser()
-            {
-                Name = "Has AlTaiar",
-                Password = "very secure passwor, my dear Alex :)",
-                Id = Guid.NewGuid().ToString()
-            };
-
-            var inserted = database.SecureInsert(user, keySeed);
-
-            var got = database.SecureGetAll<SampleUser>(keySeed);
         }
 
         private static string GenerateConnectionStringSectionWithSeed(Configuration config, string dbFilePath)
         {
             var keySeed = Convert.ToBase64String(GenerateKeyOrSalt(128));
             var connectionStringWithSalt = string.Format("Data Source={0};Password={1}", dbFilePath, keySeed);
-            config.Save();
             var sqlLiteDbconnection = config.ConnectionStrings.ConnectionStrings["SQLLiteDbconnection"];
             if (sqlLiteDbconnection == null)
             {
@@ -83,7 +99,6 @@ namespace SQLiteEncrypted
                 sqlLiteDbconnection.ConnectionString = connectionStringWithSalt;
                     
             }
-            ConfigurationManager.RefreshSection("connectionStrings");
             config.Save();
             ConnectionStringEncryptor.EncryptAppSettingsFile();
             return keySeed;
